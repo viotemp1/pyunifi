f6ba82c8993581f38a036f38e9935627c0140719
Paul Russo <paul@mtnocean.com>
2019-12-04 22:31:12 -0700

    Added to controller.py - add_radius_user(), update_radius_user(), and delete_radius_user. - _delete() and _api_delete() to support deleting RADIUS users Updated the README.md file

--- pyunifi/controller.py
+++ pyunifi/controller.py
@@ -128,6 +128,14 @@ class Controller(object):
     def _api_update(self, url, params=None):
         return self._update(self._api_url() + url, params)
 
+    @retry_login
+    def _delete(self, url, params=None):
+        r = self.session.delete(url, json=params)
+        return self._jsondec(r.text)
+
+    def _api_delete(self, url, params=None):
+        return self._delete(self._api_url() + url, params)
+
     def _login(self):
         log.debug('login() as %s', self.username)
 
@@ -257,9 +265,32 @@ class Controller(object):
 
     def get_radius_users(self):
         """Return a list of all users, with their
-        name, password, id, and site id
+        name, password, 24 digit user id, and 24 digit site id.
         """
         return self._api_read('rest/account')
+
+    def add_radius_user(self, name, password):
+        """Add a new user with this username and password.
+        :returns: user's name, password, 24 digit user id, and 24 digit site id.
+        """
+        params = {'name': name, 'x_password': password}
+        return self._api_write('rest/account/', params) 
+
+    def update_radius_user(self, name, password, id):
+        """Update a user to this new username and password.
+        Requires the user's 24 digit user id.
+        :returns: user's name, password, 24 digit user id, and 24 digit site id.
+        :returns: [] if no change was made
+        """
+        params =  {'name': name, '_id': id, 'x_password': password}
+        return self._api_update('rest/account/' + id, params) 
+    
+    def delete_radius_user(self, id):
+        """Delete user.
+        Requires the user's 24 digit user id.
+        :returns: [] if successful. 
+        """
+        return self._api_delete('rest/account/' + id)
 
     def get_switch_port_overrides(self, target_mac):
         """Gets a list of port overrides, in dictionary
