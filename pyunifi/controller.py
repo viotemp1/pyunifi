6d78528375e4dc380e620c50873a420ea1534c4b
Caleb Dunn <finish.06@gmail.com>
2020-03-30 08:57:47 -0500

    formatting fix line length

--- pyunifi/controller.py
+++ pyunifi/controller.py
@@ -79,7 +79,8 @@ class Controller(object):
 
             if ssl_verify is False:
                 warnings.simplefilter("default", category=requests.packages.
-                                      urllib3.exceptions.InsecureRequestWarning)
+                                      urllib3.exceptions.
+                                      InsecureRequestWarning)
 
             self.session = requests.Session()
             self.session.verify = ssl_verify
@@ -102,7 +103,8 @@ class Controller(object):
 
             if ssl_verify is False:
                 warnings.simplefilter("default", category=requests.packages.
-                                      urllib3.exceptions.InsecureRequestWarning)
+                                      urllib3.exceptions.
+                                      InsecureRequestWarning)
 
             self.session = requests.Session()
             self.session.verify = ssl_verify
